54a09abadfdb45785a89ba9393525b6b9955236d
Musial <daniel.musial@capgemini.com>
2019-02-18 16:47:18 +0100

    Fixed shell script syntax

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 
 apt-get -y update
 apt-get -y install apache2 
-echo \<center\>\<h1\>CCP ARM Template Demo (`hostname`)\</h1\>\<br/\>\</center\> > /var/www/html/index.html
+echo "<center><h1>CCP ARM Template Demo (`hostname`)</h1><br/></center>" > /var/www/html/index.html
 apachectl restart
